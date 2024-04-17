9c9e4639a60a4dd19821a3ab8fa421d0e8fc6372
ledsun <shigeru.nakajima@gmail.com>
2024-04-17 09:14:45 +0900

    refactor: Add pre-condition for constructor of DataSource

--- src/lib/Editor/DataSource/index.js
+++ src/lib/Editor/DataSource/index.js
@@ -6,6 +6,23 @@ export default class DataSource {
   #data
 
   constructor(type, id, data) {
+    console.assert(
+      type === 'url' ||
+        type === 'inline' ||
+        type === 'instant' ||
+        type === 'local file' ||
+        type === null,
+      'type must be url, inline, instant, local file or null.'
+    )
+    if (type === 'url') {
+      console.assert(
+        typeof id === 'string',
+        'id must be a string when type is url.'
+      )
+    } else {
+      console.assert(id === null, 'id must be null when type is not url.')
+    }
+
     this.#type = type
     this.#id = id
     this.#data = data
