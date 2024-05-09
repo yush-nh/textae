8737deb150b3da3e5539e38ea457dba9904e6473
ledsun <shigeru.nakajima@gmail.com>
2024-05-09 18:21:26 +0900

    refactor: Use private fields

--- src/lib/Editor/UseCase/OriginalData/StatusBar/index.js
+++ src/lib/Editor/UseCase/OriginalData/StatusBar/index.js
@@ -2,20 +2,23 @@ import isURI from '../../../../isURI'
 import getAreaIn from './getAreaIn'
 
 export default class StatusBar {
+  #editorHTMLElement
+  #isShow
+
   constructor(editorHTMLElement, isShow) {
-    this._editorHTMLElement = editorHTMLElement
-    this._isShow = isShow
+    this.#editorHTMLElement = editorHTMLElement
+    this.#isShow = isShow
   }
 
   set status(dataSource) {
-    if (!this._isShow) {
+    if (!this.#isShow) {
       return
     }
 
     const message = dataSource.displayName
 
     if (message !== '') {
-      getAreaIn(this._editorHTMLElement).innerHTML = isURI(message)
+      getAreaIn(this.#editorHTMLElement).innerHTML = isURI(message)
         ? `Source: ${`<a class="textae-editor__footer__message__link" href="${message}">${decodeURI(
             message
           )}</a>`}`
