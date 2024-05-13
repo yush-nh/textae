2641ebe47c2e9f27bf38ec81793abd4292970687
ledsun <shigeru.nakajima@gmail.com>
2024-05-13 13:37:58 +0900

    refactor: Extract the setLoadedAnnotation as a file

--- src/lib/Editor/UseCase/initAnnotation/index.js
+++ src/lib/Editor/UseCase/initAnnotation/index.js
@@ -1,9 +1,6 @@
-import alertifyjs from 'alertifyjs'
-import setAnnotationAndConfiguration from '../setAnnotationAndConfiguration.js'
-import validateConfigurationAndAlert from '../validateConfigurationAndAlert/index.js'
-import warningIfBeginEndOfSpanAreNotInteger from '../warningIfBeginEndOfSpanAreNotInteger/index.js'
 import DataSource from '../../DataSource.js'
 import setDefault from './setDefault.js'
+import setLoadedAnnotation from './setLoadedAnnotation.js'
 
 /**
  *
@@ -59,49 +56,5 @@ export default function (
           functionAvailability
         )
       }
-  }
-}
-
-function setLoadedAnnotation(
-  dataSource,
-  configurationURL,
-  remoteResource,
-  controlViewModel,
-  spanConfig,
-  annotationModel,
-  functionAvailability,
-  originalData
-) {
-  if (!dataSource.data.config && configurationURL) {
-    remoteResource.loadConfiguration(configurationURL, dataSource)
-  } else {
-    warningIfBeginEndOfSpanAreNotInteger(dataSource.data)
-
-    if (dataSource.data.config) {
-      // When config is specified, it must be JSON.
-      // For example, when we load an HTML file, we treat it as text here.
-      if (typeof dataSource.data.config !== 'object') {
-        alertifyjs.error(`configuration in annotation file is invalid.`)
-        return
-      }
-    }
-
-    const validConfig = validateConfigurationAndAlert(
-      dataSource.data,
-      dataSource.data.config
-    )
-
-    if (validConfig) {
-      setAnnotationAndConfiguration(
-        validConfig,
-        controlViewModel,
-        spanConfig,
-        annotationModel,
-        dataSource.data,
-        functionAvailability
-      )
-
-      originalData.annotation = dataSource
-    }
   }
 }
---
+++ src/lib/Editor/UseCase/initAnnotation/setLoadedAnnotation.js
@@ -0,0 +1,48 @@
+import alertifyjs from 'alertifyjs'
+import setAnnotationAndConfiguration from '../setAnnotationAndConfiguration.js'
+import validateConfigurationAndAlert from '../validateConfigurationAndAlert/index.js'
+import warningIfBeginEndOfSpanAreNotInteger from '../warningIfBeginEndOfSpanAreNotInteger/index.js'
+
+export default function setLoadedAnnotation(
+  dataSource,
+  configurationURL,
+  remoteResource,
+  controlViewModel,
+  spanConfig,
+  annotationModel,
+  functionAvailability,
+  originalData
+) {
+  if (!dataSource.data.config && configurationURL) {
+    remoteResource.loadConfiguration(configurationURL, dataSource)
+  } else {
+    warningIfBeginEndOfSpanAreNotInteger(dataSource.data)
+
+    if (dataSource.data.config) {
+      // When config is specified, it must be JSON.
+      // For example, when we load an HTML file, we treat it as text here.
+      if (typeof dataSource.data.config !== 'object') {
+        alertifyjs.error(`configuration in annotation file is invalid.`)
+        return
+      }
+    }
+
+    const validConfig = validateConfigurationAndAlert(
+      dataSource.data,
+      dataSource.data.config
+    )
+
+    if (validConfig) {
+      setAnnotationAndConfiguration(
+        validConfig,
+        controlViewModel,
+        spanConfig,
+        annotationModel,
+        dataSource.data,
+        functionAvailability
+      )
+
+      originalData.annotation = dataSource
+    }
+  }
+}
