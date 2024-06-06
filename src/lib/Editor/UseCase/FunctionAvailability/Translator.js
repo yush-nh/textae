e42cca753498e0f2e8c68c0a567940253b4d4115
ledsun <shigeru.nakajima@gmail.com>
2024-06-06 15:20:59 +0900

    refactor: Change data structure

--- src/lib/Editor/UseCase/FunctionAvailability/Translator.js
+++ src/lib/Editor/UseCase/FunctionAvailability/Translator.js
@@ -6,45 +6,45 @@ export default class Translator {
 
   constructor() {
     this.#map = new Map([
-      ['logo', 'show logo'],
-      ['read', 'import'],
-      ['write', 'upload'],
-      ['write-auto', 'upload automatically'],
-      ['view', 'view mode'],
-      ['term', 'edit term mode'],
-      ['block', 'edit block mode'],
-      ['relation', 'edit relation mode'],
-      ['text-edit', 'edit text mode'],
-      ['simple', 'simple view'],
-      ['line-height', 'adjust lineheight'],
-      ['line-height-auto', 'auto adjust lineheight'],
-      ['undo', 'undo'],
-      ['redo', 'redo'],
-      ['replicate', 'replicate span annotation'],
-      ['replicate-auto', 'auto replicate'],
-      ['boundary-detection', 'boundary detection'],
-      ['create-span-by-touch', 'create span by touch'],
-      ['expand-span-by-touch', 'expand span by touch'],
-      ['shrink-span-by-touch', 'shrink span by touch'],
-      ['entity', 'new entity'],
-      ['pallet', 'pallet'],
-      ['edit-properties', 'edit properties'],
-      ['delete', 'delete'],
-      ['copy', 'copy'],
-      ['cut', 'cut'],
-      ['paste', 'paste'],
-      ['setting', 'setting'],
-      ['help', 'help']
+      ['logo', { name: 'show logo', enabled: true }],
+      ['read', { name: 'import', enabled: true }],
+      ['write', { name: 'upload', enabled: true }],
+      ['write-auto', { name: 'upload automatically', enabled: true }],
+      ['view', { name: 'view mode', enabled: true }],
+      ['term', { name: 'edit term mode', enabled: true }],
+      ['block', { name: 'edit block mode', enabled: true }],
+      ['relation', { name: 'edit relation mode', enabled: true }],
+      ['text-edit', { name: 'edit text mode', enabled: true }],
+      ['simple', { name: 'simple view', enabled: true }],
+      ['line-height', { name: 'adjust lineheight', enabled: true }],
+      ['line-height-auto', { name: 'auto adjust lineheight', enabled: true }],
+      ['undo', { name: 'undo', enabled: true }],
+      ['redo', { name: 'redo', enabled: true }],
+      ['replicate', { name: 'replicate span annotation', enabled: true }],
+      ['replicate-auto', { name: 'auto replicate', enabled: true }],
+      ['boundary-detection', { name: 'boundary detection', enabled: true }],
+      ['create-span-by-touch', { name: 'create span by touch', enabled: true }],
+      ['expand-span-by-touch', { name: 'expand span by touch', enabled: true }],
+      ['shrink-span-by-touch', { name: 'shrink span by touch', enabled: true }],
+      ['entity', { name: 'new entity', enabled: true }],
+      ['pallet', { name: 'pallet', enabled: true }],
+      ['edit-properties', { name: 'edit properties', enabled: true }],
+      ['delete', { name: 'delete', enabled: true }],
+      ['copy', { name: 'copy', enabled: true }],
+      ['cut', { name: 'cut', enabled: true }],
+      ['paste', { name: 'paste', enabled: true }],
+      ['setting', { name: 'setting', enabled: true }],
+      ['help', { name: 'help', enabled: true }]
     ])
   }
 
   get innerNames() {
-    return this.#map.values()
+    return this.#map.values().map((value) => value.name)
   }
 
   translateToInnerNameFrom(functionName) {
     if (this.#map.has(functionName)) {
-      return this.#map.get(functionName)
+      return this.#map.get(functionName).name
     }
 
     alertifyjs.warning(
