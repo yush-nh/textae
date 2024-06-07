2be23c074bfa22c0dbd9ca47346a7cb7bd5d209a
ledsun <shigeru.nakajima@gmail.com>
2024-06-07 16:49:03 +0900

    refactor: Inline the bindMouseEvents

--- src/lib/Editor/UseCase/Presenter/EditModeSwitch/RelationEditMode/MouseEventHandler/index.js
+++ src/lib/Editor/UseCase/Presenter/EditModeSwitch/RelationEditMode/MouseEventHandler/index.js
@@ -1,4 +1,5 @@
-import bindMouseEvents from '../bindMouseEvents'
+import delegate from 'delegate'
+import getEntityHTMLelementFromChild from '../../../../getEntityHTMLelementFromChild'
 import typeValuesClicked from './typeValuesClicked'
 
 export default class MouseEventHandler {
@@ -23,19 +24,66 @@ export default class MouseEventHandler {
   }
 
   bind() {
-    return bindMouseEvents(this.#editorHTMLElement, this)
+    const listeners = []
+
+    // In relation mode does not manipulate the child elements in the text box.
+    listeners.push(
+      delegate(
+        this.#editorHTMLElement,
+        '.textae-editor__text-box',
+        'click',
+        () => this.#bodyClicked()
+      )
+    )
+
+    listeners.push(
+      delegate(this.#editorHTMLElement, '.textae-editor', 'click', (e) => {
+        // The delegate also fires events for child elements of the selector.
+        // Ignores events that occur in child elements.
+        // Otherwise, you cannot select child elements.
+        if (e.target.classList.contains('textae-editor')) {
+          this.#bodyClicked()
+        }
+      })
+    )
+
+    listeners.push(
+      // When a relation is selected, the HTML element of the relation is recreated,
+      // so the click event is not fired on the parent element.
+      delegate(
+        this.#editorHTMLElement,
+        '.textae-editor__signboard',
+        'mousedown',
+        () => this.#signboardClicked()
+      )
+    )
+
+    listeners.push(
+      delegate(
+        this.#editorHTMLElement,
+        '.textae-editor__signboard__type-values',
+        'click',
+        (event) => {
+          const entityID = getEntityHTMLelementFromChild(event.target).dataset
+            .id
+          this.#typeValuesClicked(event, entityID)
+        }
+      )
+    )
+
+    return listeners
   }
 
-  bodyClicked() {
+  #bodyClicked() {
     this.#pallet.hide()
     this.#selectionModel.removeAll()
   }
 
-  signboardClicked() {
+  #signboardClicked() {
     this.#editorHTMLElement.focus()
   }
 
-  typeValuesClicked(event, entityID) {
+  #typeValuesClicked(event, entityID) {
     typeValuesClicked(
       this.#selectionModel,
       this.#commander,
