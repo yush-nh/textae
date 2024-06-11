ed585e83158f7b88bd04461e076187edb38910e6
ledsun <shigeru.nakajima@gmail.com>
2024-06-11 14:06:51 +0900

    refactor: Remove an argument

--- src/lib/Editor/UseCase/Presenter/EditModeSwitch/shrinkSpan/index.js
+++ src/lib/Editor/UseCase/Presenter/EditModeSwitch/shrinkSpan/index.js
@@ -24,7 +24,6 @@ export default function (
       commander,
       spanAdjuster,
       spanId,
-      selectionWrapper,
       spanConfig,
       moveHandler
     )
--- src/lib/Editor/UseCase/Presenter/EditModeSwitch/shrinkSpan/shrinkSpanToSelection.js
+++ src/lib/Editor/UseCase/Presenter/EditModeSwitch/shrinkSpan/shrinkSpanToSelection.js
@@ -10,7 +10,6 @@ export default function (
   commander,
   spanAdjuster,
   spanId,
-  selectionWrapper,
   spanConfig,
   moveHandler
 ) {
