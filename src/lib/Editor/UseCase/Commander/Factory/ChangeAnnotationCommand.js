601f22d7c07cf107d389710e3e09687693517799
ledsun <shigeru.nakajima@gmail.com>
2024-06-12 18:20:47 +0900

    refactor: Use private fields

--- src/lib/Editor/UseCase/Commander/Factory/ChangeAnnotationCommand.js
+++ src/lib/Editor/UseCase/Commander/Factory/ChangeAnnotationCommand.js
@@ -2,35 +2,40 @@ import commandLog from './commandLog'
 import AnnotationCommand from './AnnotationCommand'
 
 export default class ChangeAnnotationCommand extends AnnotationCommand {
+  #annotationDataModel
+  #annotationType
+  #id
+  #newType
+
   constructor(annotationModel, annotationType, id, newType) {
     super()
-    this._annotationModel = annotationModel
-    this._annotationType = annotationType
-    this._id = id
-    this._newType = newType
+    this.#annotationDataModel = annotationModel
+    this.#annotationType = annotationType
+    this.#id = id
+    this.#newType = newType
   }
 
   execute() {
-    this.oldType = this._annotationModel
-      .getInstanceContainerFor(this._annotationType)
-      .get(this._id).typeName
+    this.oldType = this.#annotationDataModel
+      .getInstanceContainerFor(this.#annotationType)
+      .get(this.#id).typeName
 
     // Update instance
-    const targetInstance = this._annotationModel
-      .getInstanceContainerFor(this._annotationType)
-      .changeType(this._id, this._newType)
+    const targetInstance = this.#annotationDataModel
+      .getInstanceContainerFor(this.#annotationType)
+      .changeType(this.#id, this.#newType)
     commandLog(
       this,
-      `change type of a ${this._annotationType}. old type:${this.oldType} ${this._annotationType}:`,
+      `change type of a ${this.#annotationType}. old type:${this.oldType} ${this.#annotationType}:`,
       targetInstance
     )
   }
 
   revert() {
     return new ChangeAnnotationCommand(
-      this._annotationModel,
-      this._annotationType,
-      this._id,
+      this.#annotationDataModel,
+      this.#annotationType,
+      this.#id,
       this.oldType
     )
   }
