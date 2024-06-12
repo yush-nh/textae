869003ca969ff25bba250e680e1397e18315429d
ledsun <shigeru.nakajima@gmail.com>
2024-06-12 14:08:36 +0900

    refactor: Use private fields and methods

--- src/lib/Editor/AnnotationModel/SpanInstanceContainer/index.js
+++ src/lib/Editor/AnnotationModel/SpanInstanceContainer/index.js
@@ -12,20 +12,29 @@ import rangeFrom from './rangeFrom'
 import getCurrentMaxHeight from './getCurrentMaxHeight'
 
 export default class SpanInstanceContainer {
+  #editorID
+  #editorHTMLElement
+  #emitter
+  #entityContainer
+  #textBox
+  #denotations
+  #blocks
+  #styles
+
   /**
    *
    * @param {import('../createTextBox/TextBox').default} textBox
    */
   constructor(editorID, editorHTMLElement, emitter, entityContainer, textBox) {
-    this._editorID = editorID
-    this._editorHTMLElement = editorHTMLElement
-    this._emitter = emitter
-    this._entityContainer = entityContainer
-    this._textBox = textBox
+    this.#editorID = editorID
+    this.#editorHTMLElement = editorHTMLElement
+    this.#emitter = emitter
+    this.#entityContainer = entityContainer
+    this.#textBox = textBox
 
-    this._denotations = new Map()
-    this._blocks = new Map()
-    this._styles = new Map()
+    this.#denotations = new Map()
+    this.#blocks = new Map()
+    this.#styles = new Map()
   }
 
   // expected span is like { "begin": 19, "end": 49 }
@@ -35,9 +44,9 @@ export default class SpanInstanceContainer {
     // When redoing, the newValue is instance of the BlockSpanInstance
     // or the DenotationSpan already.
     if (newValue instanceof BlockSpanInstance) {
-      return this._addBlock(newValue)
+      return this.#addBlock(newValue)
     } else if (newValue instanceof DenotationSpanInstance) {
-      return this._addDenotation(newValue)
+      return this.#addDenotation(newValue)
     } else if (newValue.isBlock) {
       console.assert(
         !this.doesParentOrSameSpanExist(newValue.begin, newValue.end),
@@ -45,29 +54,29 @@ export default class SpanInstanceContainer {
       )
 
       const blockSpan = new BlockSpanInstance(
-        this._editorID,
-        this._editorHTMLElement,
+        this.#editorID,
+        this.#editorHTMLElement,
         newValue.begin,
         newValue.end,
-        this._entityContainer,
+        this.#entityContainer,
         this,
-        this._textBox
+        this.#textBox
       )
-      return this._addBlock(blockSpan)
+      return this.#addBlock(blockSpan)
     } else {
       console.assert(
         !this.hasDenotationSpan(newValue.begin, newValue.end),
         'There is already a span.'
       )
       const denotationSpan = new DenotationSpanInstance(
-        this._editorID,
-        this._editorHTMLElement,
+        this.#editorID,
+        this.#editorHTMLElement,
         newValue.begin,
         newValue.end,
-        this._entityContainer,
+        this.#entityContainer,
         this
       )
-      return this._addDenotation(denotationSpan)
+      return this.#addDenotation(denotationSpan)
     }
   }
 
@@ -76,24 +85,24 @@ export default class SpanInstanceContainer {
   // The drawing of the instance is performed at a different time.
   addSource(source, type) {
     for (const element of source) {
-      this._addInstanceFromElement(type, element)
+      this.#addInstanceFromElement(type, element)
     }
 
-    this._updateSpanTree()
+    this.#updateSpanTree()
   }
 
   hasDenotationSpan(begin, end) {
-    const spanID = makeDenotationSpanHTMLElementID(this._editorID, begin, end)
-    return this._denotations.has(spanID)
+    const spanID = makeDenotationSpanHTMLElementID(this.#editorID, begin, end)
+    return this.#denotations.has(spanID)
   }
 
   hasBlockSpan(begin, end) {
-    const spanID = makeBlockSpanHTMLElementID(this._editorID, begin, end)
-    return this._blocks.has(spanID)
+    const spanID = makeBlockSpanHTMLElementID(this.#editorID, begin, end)
+    return this.#blocks.has(spanID)
   }
 
   hasBlockSpanBetween(begin, end, option = {}) {
-    for (const blockSpan of this._blocks.values()) {
+    for (const blockSpan of this.#blocks.values()) {
       if (
         begin <= blockSpan.begin &&
         blockSpan.end <= end &&
@@ -126,36 +135,36 @@ export default class SpanInstanceContainer {
    * @returns {import('./SpanInstance').SpanInstance}
    */
   get(spanID) {
-    if (this._denotations.has(spanID)) {
-      return this._denotations.get(spanID)
-    } else if (this._blocks.has(spanID)) {
-      return this._blocks.get(spanID)
+    if (this.#denotations.has(spanID)) {
+      return this.#denotations.get(spanID)
+    } else if (this.#blocks.has(spanID)) {
+      return this.#blocks.get(spanID)
     } else {
       // Returns a typesetting only.
-      return this._styles.get(spanID)
+      return this.#styles.get(spanID)
     }
   }
 
   getStyle(spanID) {
-    if (this._styles.has(spanID)) {
-      return this._styles.get(spanID).styles
+    if (this.#styles.has(spanID)) {
+      return this.#styles.get(spanID).styles
     } else {
       return new Set()
     }
   }
 
   getDenotationSpan(spanID) {
-    if (this._denotations.has(spanID)) {
-      return this._denotations.get(spanID)
+    if (this.#denotations.has(spanID)) {
+      return this.#denotations.get(spanID)
     }
   }
 
   rangeDenotationSpan(firstID, secondID) {
-    return rangeFrom(this._denotations, firstID, secondID)
+    return rangeFrom(this.#denotations, firstID, secondID)
   }
 
   rangeBlockSpan(firstID, secondID) {
-    return rangeFrom(this._blocks, firstID, secondID)
+    return rangeFrom(this.#blocks, firstID, secondID)
   }
 
   get topLevel() {
@@ -167,23 +176,23 @@ export default class SpanInstanceContainer {
   }
 
   clear() {
-    this._denotations.clear()
-    this._blocks.clear()
-    this._styles.clear()
+    this.#denotations.clear()
+    this.#blocks.clear()
+    this.#styles.clear()
   }
 
   remove(id) {
-    const blockSpan = this._blocks.get(id)
+    const blockSpan = this.#blocks.get(id)
     if (blockSpan) {
-      this._removeBlock(blockSpan)
-      this._emitter.emit(`textae-event.annotation-data.span.remove`, blockSpan)
+      this.#removeBlock(blockSpan)
+      this.#emitter.emit(`textae-event.annotation-data.span.remove`, blockSpan)
       return
     }
 
-    const denotationSpan = this._denotations.get(id)
+    const denotationSpan = this.#denotations.get(id)
     if (denotationSpan) {
-      this._removeDenotation(denotationSpan)
-      this._emitter.emit(
+      this.#removeDenotation(denotationSpan)
+      this.#emitter.emit(
         `textae-event.annotation-data.span.remove`,
         denotationSpan
       )
@@ -197,25 +206,25 @@ export default class SpanInstanceContainer {
   // we will delete and add the instance as well.
   moveDenotationSpan(id, begin, end) {
     console.assert(
-      id !== makeDenotationSpanHTMLElementID(this._editorID, begin, end),
+      id !== makeDenotationSpanHTMLElementID(this.#editorID, begin, end),
       `Do not need move span:  ${id} ${begin} ${end}`
     )
 
-    const oldSpan = this._denotations.get(id)
+    const oldSpan = this.#denotations.get(id)
     console.assert(oldSpan, `There is no target for move for ${id}!`)
 
-    this._removeDenotation(oldSpan)
+    this.#removeDenotation(oldSpan)
 
     const newOne = new DenotationSpanInstance(
-      this._editorID,
-      this._editorHTMLElement,
+      this.#editorID,
+      this.#editorHTMLElement,
       begin,
       end,
-      this._entityContainer,
+      this.#entityContainer,
       this
     )
-    this._addDenotation(newOne, oldSpan)
-    this._emitter.emit('textae-event.annotation-data.span.move')
+    this.#addDenotation(newOne, oldSpan)
+    this.#emitter.emit('textae-event.annotation-data.span.move')
 
     return {
       begin: oldSpan.begin,
@@ -226,24 +235,24 @@ export default class SpanInstanceContainer {
 
   moveBlockSpan(id, begin, end) {
     console.assert(
-      id !== makeBlockSpanHTMLElementID(this._editorID, begin, end),
+      id !== makeBlockSpanHTMLElementID(this.#editorID, begin, end),
       `Do not need move span:  ${id} ${begin} ${end}`
     )
 
-    const oldSpan = this._blocks.get(id)
-    this._removeBlock(oldSpan)
+    const oldSpan = this.#blocks.get(id)
+    this.#removeBlock(oldSpan)
 
     const newOne = new BlockSpanInstance(
-      this._editorID,
-      this._editorHTMLElement,
+      this.#editorID,
+      this.#editorHTMLElement,
       begin,
       end,
-      this._entityContainer,
+      this.#entityContainer,
       this,
-      this._textBox
+      this.#textBox
     )
-    this._addBlock(newOne, oldSpan)
-    this._emitter.emit('textae-event.annotation-data.span.move')
+    this.#addBlock(newOne, oldSpan)
+    this.#emitter.emit('textae-event.annotation-data.span.move')
 
     return {
       begin: oldSpan.begin,
@@ -252,23 +261,23 @@ export default class SpanInstanceContainer {
     }
   }
 
-  _addDenotation(denotationSpan, oldSpan = null) {
-    this._addSpan(this._denotations, denotationSpan, oldSpan)
-    this._emitter.emit(`textae-event.annotation-data.span.add`, denotationSpan)
+  #addDenotation(denotationSpan, oldSpan = null) {
+    this.#addSpan(this.#denotations, denotationSpan, oldSpan)
+    this.#emitter.emit(`textae-event.annotation-data.span.add`, denotationSpan)
 
     return denotationSpan
   }
 
-  _addBlock(blockSpan, oldSpan = null) {
-    this._addSpan(this._blocks, blockSpan, oldSpan)
-    this._emitter.emit(`textae-event.annotation-data.span.add`, blockSpan)
+  #addBlock(blockSpan, oldSpan = null) {
+    this.#addSpan(this.#blocks, blockSpan, oldSpan)
+    this.#emitter.emit(`textae-event.annotation-data.span.add`, blockSpan)
 
     return blockSpan
   }
 
-  _addSpan(container, span, oldSpan = null) {
+  #addSpan(container, span, oldSpan = null) {
     container.set(span.id, span)
-    this._updateSpanTree()
+    this.#updateSpanTree()
 
     if (oldSpan) {
       // Span.entities depends on the property of the entity.
@@ -283,16 +292,16 @@ export default class SpanInstanceContainer {
     span.drawGrid(clientHeight, clientWidth)
   }
 
-  _removeDenotation(span) {
-    this._denotations.delete(span.id)
+  #removeDenotation(span) {
+    this.#denotations.delete(span.id)
     span.erase()
     // When changing the length of a span, the span is erased and rendered again.
     // When the span is erased, the span erase event fires and the position calculations for all annotations are performed.
     // The event is not fired in this function.
   }
 
-  _removeBlock(span) {
-    this._blocks.delete(span.id)
+  #removeBlock(span) {
+    this.#blocks.delete(span.id)
     span.erase()
     // When changing the length of a span, the span is erased and rendered again.
     // When the span is erased, the span erase event fires and the position calculations for all annotations are performed.
@@ -307,24 +316,24 @@ export default class SpanInstanceContainer {
     const isParent = (span) => span.begin <= begin && end <= span.end
 
     return (
-      [...this._denotations.values()].some(isParent) ||
-      [...this._blocks.values()].some(isParent) ||
-      [...this._styles.values()].some(isParent)
+      [...this.#denotations.values()].some(isParent) ||
+      [...this.#blocks.values()].some(isParent) ||
+      [...this.#styles.values()].some(isParent)
     )
   }
 
   get all() {
-    const styleOnlySpans = [...this._styles.values()].filter(
-      (s) => !this._denotations.has(s.id)
+    const styleOnlySpans = [...this.#styles.values()].filter(
+      (s) => !this.#denotations.has(s.id)
     )
-    return [...this._blocks.values()]
-      .concat([...this._denotations.values()])
+    return [...this.#blocks.values()]
+      .concat([...this.#denotations.values()])
       .concat(styleOnlySpans)
   }
 
   get selectedItems() {
-    return [...this._blocks.values()]
-      .concat([...this._denotations.values()])
+    return [...this.#blocks.values()]
+      .concat([...this.#denotations.values()])
       .filter(({ isSelected }) => isSelected)
   }
 
@@ -332,11 +341,11 @@ export default class SpanInstanceContainer {
    * @returns {import('./DenotationSpanInstance').DenotationSpanInstance[]}
    */
   get allDenotationSpans() {
-    return [...this._denotations.values()]
+    return [...this.#denotations.values()]
   }
 
   get allBlockSpans() {
-    return [...this._blocks.values()]
+    return [...this.#blocks.values()]
   }
 
   // It has a common interface with the span instance so that it can be the parent of the span instance.
@@ -346,51 +355,51 @@ export default class SpanInstanceContainer {
 
   // It has a common interface with the span instance so that it can be the parent of the span instance
   get element() {
-    return this._editorHTMLElement.querySelector(`.textae-editor__text-box`)
+    return this.#editorHTMLElement.querySelector(`.textae-editor__text-box`)
   }
 
-  _updateSpanTree() {
+  #updateSpanTree() {
     // Register a typesetting in the span tree to put it in the span rendering flow.
     updateSpanTree(this, this.all)
   }
 
-  _addInstanceFromElement(type, denotation) {
+  #addInstanceFromElement(type, denotation) {
     switch (type) {
       case 'denotation': {
         const objectSpan = new DenotationSpanInstance(
-          this._editorID,
-          this._editorHTMLElement,
+          this.#editorID,
+          this.#editorHTMLElement,
           denotation.span.begin,
           denotation.span.end,
-          this._entityContainer,
+          this.#entityContainer,
           this
         )
 
-        if (!this._denotations.has(objectSpan.id)) {
-          this._denotations.set(objectSpan.id, objectSpan)
+        if (!this.#denotations.has(objectSpan.id)) {
+          this.#denotations.set(objectSpan.id, objectSpan)
         }
         break
       }
       case 'block': {
         const blockSpan = new BlockSpanInstance(
-          this._editorID,
-          this._editorHTMLElement,
+          this.#editorID,
+          this.#editorHTMLElement,
           denotation.span.begin,
           denotation.span.end,
-          this._entityContainer,
+          this.#entityContainer,
           this,
-          this._textBox
+          this.#textBox
         )
 
-        if (!this._blocks.has(blockSpan.id)) {
-          this._blocks.set(blockSpan.id, blockSpan)
+        if (!this.#blocks.has(blockSpan.id)) {
+          this.#blocks.set(blockSpan.id, blockSpan)
         }
         break
       }
       case 'typesetting': {
         const styleSpan = new StyleSpanInstance(
-          this._editorID,
-          this._editorHTMLElement,
+          this.#editorID,
+          this.#editorHTMLElement,
           denotation.span.begin,
           denotation.span.end,
           this,
@@ -398,10 +407,10 @@ export default class SpanInstanceContainer {
         )
 
         // Merge multiple styles for the same range.
-        if (this._styles.has(styleSpan.id)) {
-          this._styles.get(styleSpan.id).appendStyles(styleSpan.styles)
+        if (this.#styles.has(styleSpan.id)) {
+          this.#styles.get(styleSpan.id).appendStyles(styleSpan.styles)
         } else {
-          this._styles.set(styleSpan.id, styleSpan)
+          this.#styles.set(styleSpan.id, styleSpan)
         }
 
         break
@@ -430,8 +439,8 @@ export default class SpanInstanceContainer {
   }
 
   get maxHeight() {
-    const spans = [...this._blocks.values()].concat([
-      ...this._denotations.values()
+    const spans = [...this.#blocks.values()].concat([
+      ...this.#denotations.values()
     ])
 
     if (spans.length) {
