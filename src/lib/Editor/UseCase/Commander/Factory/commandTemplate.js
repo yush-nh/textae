f9ff1f1695c07dd66cc02f3567ca9a235cd01bd6
ledsun <shigeru.nakajima@gmail.com>
2024-06-12 18:18:08 +0900

    refactor: Use private fields

--- src/lib/Editor/UseCase/Commander/Factory/commandTemplate.js
+++ src/lib/Editor/UseCase/Commander/Factory/commandTemplate.js
@@ -2,6 +2,11 @@ import commandLog from './commandLog'
 import AnnotationCommand from './AnnotationCommand'
 
 class CreateCommand extends AnnotationCommand {
+  #annotationModel
+  #annotationType
+  #instance
+  #selectionModel
+
   constructor(
     annotationModel,
     annotationType,
@@ -9,29 +14,29 @@ class CreateCommand extends AnnotationCommand {
     selectionModel = null
   ) {
     super()
-    this._annotationModel = annotationModel
-    this._annotationType = annotationType
-    this._instance = instance
-    this._selectionModel = selectionModel
+    this.#annotationModel = annotationModel
+    this.#annotationType = annotationType
+    this.#instance = instance
+    this.#selectionModel = selectionModel
   }
 
   execute() {
-    this._instance = this._annotationModel
-      .getInstanceContainerFor(this._annotationType)
-      .add(this._instance)
+    this.#instance = this.#annotationModel
+      .getInstanceContainerFor(this.#annotationType)
+      .add(this.#instance)
 
-    if (this._selectionModel) {
-      this._selectionModel.add(this._annotationType, [this._instance.id])
+    if (this.#selectionModel) {
+      this.#selectionModel.add(this.#annotationType, [this.#instance.id])
     }
 
-    commandLog(this, `${this._annotationType}: ${this._instance.id}`)
+    commandLog(this, `${this.#annotationType}: ${this.#instance.id}`)
   }
 
   revert() {
     return new RemoveCommand(
-      this._annotationModel,
-      this._annotationType,
-      this._instance
+      this.#annotationModel,
+      this.#annotationType,
+      this.#instance
     )
   }
 }
