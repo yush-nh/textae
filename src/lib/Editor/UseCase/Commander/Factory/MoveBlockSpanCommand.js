90f178c5ffb992e27e5eb91619e396269429df44
ledsun <shigeru.nakajima@gmail.com>
2024-06-14 13:48:49 +0900

    refactor: use private field and rename

--- src/lib/Editor/UseCase/Commander/Factory/MoveBlockSpanCommand.js
+++ src/lib/Editor/UseCase/Commander/Factory/MoveBlockSpanCommand.js
@@ -2,39 +2,47 @@ import commandLog from './commandLog'
 import AnnotationCommand from './AnnotationCommand'
 
 export default class MoveBlockSpanCommand extends AnnotationCommand {
-  constructor(annotationModel, spanId, begin, end) {
+  #annotationModel
+  #spanID
+  #begin
+  #end
+  #newID
+  #beginBeforeMove
+  #endBeforeMove
+
+  constructor(annotationModel, spanID, begin, end) {
     super()
-    this._annotationModel = annotationModel
-    this._spanId = spanId
-    this._begin = begin
-    this._end = end
+    this.#annotationModel = annotationModel
+    this.#spanID = spanID
+    this.#begin = begin
+    this.#end = end
   }
 
   execute() {
     // Update instance.
     const { id, begin, end } =
-      this._annotationModel.spanInstanceContainer.moveBlockSpan(
-        this._spanId,
-        this._begin,
-        this._end
+      this.#annotationModel.spanInstanceContainer.moveBlockSpan(
+        this.#spanID,
+        this.#begin,
+        this.#end
       )
 
-    this._newId = id
-    this._oldBegin = begin
-    this._oldEnd = end
+    this.#newID = id
+    this.#beginBeforeMove = begin
+    this.#endBeforeMove = end
 
     commandLog(
       this,
-      `move span: ${this._spanId} to {begin: ${this._begin}, end: ${this._end}}`
+      `move span: ${this.#spanID} to {begin: ${this.#begin}, end: ${this.#end}}`
     )
   }
 
   revert() {
     return new MoveBlockSpanCommand(
-      this._annotationModel,
-      this._newId,
-      this._oldBegin,
-      this._oldEnd
+      this.#annotationModel,
+      this.#newID,
+      this.#beginBeforeMove,
+      this.#endBeforeMove
     )
   }
 }
