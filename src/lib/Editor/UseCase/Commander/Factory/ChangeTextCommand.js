49add17cca7492dfa00292a39ec3f8bb2ee3af04
ledsun <shigeru.nakajima@gmail.com>
2024-06-14 11:21:55 +0900

    refactor: Add ChangeTextCommand class

---
+++ src/lib/Editor/UseCase/Commander/Factory/ChangeTextCommand.js
@@ -0,0 +1,41 @@
+import AnnotationCommand from './AnnotationCommand'
+import commandLog from './commandLog'
+
+export default class ChangeTextCommand extends AnnotationCommand {
+  #annotationModel
+  #begin
+  #end
+  #newText
+  #endAfterChange
+  #textBeforeChange
+
+  constructor(annotationModel, targetTextSelection, newText) {
+    super()
+
+    this.#annotationModel = annotationModel
+    this.#begin = targetTextSelection.begin
+    this.#end = targetTextSelection.end
+    this.#newText = newText
+  }
+
+  execute() {
+    this.#endAfterChange = this.#begin + this.#newText.length
+    this.#textBeforeChange = this.#annotationModel.sourceDoc.substring(
+      this.#begin,
+      this.#end
+    )
+
+    commandLog(
+      this,
+      `change text at ${this.#begin}:${this.#end} to ${this.#newText}`
+    )
+  }
+
+  revert() {
+    return new ChangeTextCommand(
+      this.#annotationModel,
+      { begin: this.#begin, end: this.#endAfterChange },
+      this.#textBeforeChange
+    )
+  }
+}
