1a257cd5a383e72d49dbefd141a76f97e5013c03
ledsun <shigeru.nakajima@gmail.com>
2024-06-17 11:53:11 +0900

    refactor: Use private field

--- src/lib/Editor/UseCase/Presenter/EditModeSwitch/AttributeEditor/index.js
+++ src/lib/Editor/UseCase/Presenter/EditModeSwitch/AttributeEditor/index.js
@@ -3,6 +3,14 @@ import createNumericAttributeOrShowEditNumericAttributeDialog from './createNume
 import createStringAttributeOrShowEditStringAttributeDialog from './createStringAttributeOrShowEditStringAttributeDialog'
 
 export default class AttributeEditor {
+  #commander
+  #annotationModel
+  #selectionModelItems
+  #selectionAttributePallet
+  #typeDefinition
+  #editProperties
+  #typeValuesPallet
+
   constructor(
     commander,
     annotationModel,
@@ -11,19 +19,19 @@ export default class AttributeEditor {
     editProperties,
     typeValuesPallet
   ) {
-    this._commander = commander
-    this._annotationModel = annotationModel
-    this._selectionModelItems = selectionModelItems
-    this._selectionAttributePallet = selectionAttributePallet
-    this._typeDefinition = annotationModel.typeDefinition
-    this._editProperties = editProperties
-    this._typeValuesPallet = typeValuesPallet
+    this.#commander = commander
+    this.#annotationModel = annotationModel
+    this.#selectionModelItems = selectionModelItems
+    this.#selectionAttributePallet = selectionAttributePallet
+    this.#typeDefinition = annotationModel.typeDefinition
+    this.#editProperties = editProperties
+    this.#typeValuesPallet = typeValuesPallet
   }
 
   addOrEditAt(number) {
-    this._selectionAttributePallet.hide()
+    this.#selectionAttributePallet.hide()
 
-    const attrDef = this._typeDefinition.attribute.getAttributeAt(number)
+    const attrDef = this.#typeDefinition.attribute.getAttributeAt(number)
 
     if (!attrDef) {
       alertifyjs.warning(`Attribute No.${number} is not defined`)
@@ -32,28 +40,28 @@ export default class AttributeEditor {
 
     switch (attrDef.valueType) {
       case 'flag':
-        this._commander.invoke(
-          this._commander.factory.toggleFlagAttributeToItemsCommand(
-            this._selectionModelItems.all,
+        this.#commander.invoke(
+          this.#commander.factory.toggleFlagAttributeToItemsCommand(
+            this.#selectionModelItems.all,
             attrDef
           )
         )
         break
       case 'numeric':
         createNumericAttributeOrShowEditNumericAttributeDialog(
-          this._selectionModelItems,
+          this.#selectionModelItems,
           attrDef,
-          this._commander,
-          this._editProperties,
-          this._typeValuesPallet
+          this.#commander,
+          this.#editProperties,
+          this.#typeValuesPallet
         )
         break
       case 'selection':
         {
-          if (this._selectionModelItems.selectedWithAttributeOf(attrDef.pred)) {
-            this._selectionAttributePallet.show(attrDef).then((newObj) => {
+          if (this.#selectionModelItems.selectedWithAttributeOf(attrDef.pred)) {
+            this.#selectionAttributePallet.show(attrDef).then((newObj) => {
               if (
-                this._selectionModelItems.isDuplicatedPredAttributeSelected(
+                this.#selectionModelItems.isDuplicatedPredAttributeSelected(
                   attrDef.pred
                 )
               ) {
@@ -62,31 +70,31 @@ export default class AttributeEditor {
                 )
               } else {
                 const command =
-                  this._commander.factory.changeAttributeObjOfItemsCommand(
-                    this._selectionModelItems.all,
+                  this.#commander.factory.changeAttributeObjOfItemsCommand(
+                    this.#selectionModelItems.all,
                     attrDef,
                     newObj
                   )
-                this._commander.invoke(command)
+                this.#commander.invoke(command)
               }
             })
           } else {
             const command =
-              this._commander.factory.createAttributeToItemsCommand(
-                this._selectionModelItems.all,
+              this.#commander.factory.createAttributeToItemsCommand(
+                this.#selectionModelItems.all,
                 attrDef
               )
-            this._commander.invoke(command)
+            this.#commander.invoke(command)
           }
         }
         break
       case 'string':
         createStringAttributeOrShowEditStringAttributeDialog(
-          this._selectionModelItems,
+          this.#selectionModelItems,
           attrDef,
-          this._commander,
-          this._editProperties,
-          this._typeValuesPallet
+          this.#commander,
+          this.#editProperties,
+          this.#typeValuesPallet
         )
         break
       default:
@@ -95,20 +103,20 @@ export default class AttributeEditor {
   }
 
   deleteAt(number) {
-    const attrDef = this._typeDefinition.attribute.getAttributeAt(number)
+    const attrDef = this.#typeDefinition.attribute.getAttributeAt(number)
 
     if (!attrDef) {
       alertifyjs.warning(`Attribute No.${number} is not defined`)
       return
     }
 
-    if (this._selectionModelItems.selectedWithAttributeOf(attrDef.pred)) {
+    if (this.#selectionModelItems.selectedWithAttributeOf(attrDef.pred)) {
       const command =
-        this._commander.factory.removeAttributesFromItemsByPredCommand(
-          this._selectionModelItems.all,
+        this.#commander.factory.removeAttributesFromItemsByPredCommand(
+          this.#selectionModelItems.all,
           attrDef
         )
-      this._commander.invoke(command)
+      this.#commander.invoke(command)
     } else {
       alertifyjs.warning('None of the selected items has this attribute.')
     }
