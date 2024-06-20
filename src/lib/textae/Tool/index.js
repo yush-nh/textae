13cb6a9e0596244aefe74201b8ae47a677e0261a
ledsun <shigeru.nakajima@gmail.com>
2024-06-20 13:56:48 +0900

    refactor: Use private field

--- src/lib/textae/Tool/index.js
+++ src/lib/textae/Tool/index.js
@@ -6,38 +6,42 @@ import getMousePoint from './getMousePoint'
 
 // The tool manages interactions between components.
 export default class Tool {
+  #editorContainer
+  #veil
+  #mousePoint
+
   constructor() {
-    this._editorContainer = new EditorContainer()
-    this._veil = new Veil()
-    this._mousePoint = getMousePoint()
+    this.#editorContainer = new EditorContainer()
+    this.#veil = new Veil()
+    this.#mousePoint = getMousePoint()
 
     // When the DOMContentLoaded event occurs, document.body may not have been initialized yet.
     // When the load event occurs, bind the event handler of document.body.
     window.addEventListener('load', () =>
-      observeBodyEvents(this._editorContainer)
+      observeBodyEvents(this.#editorContainer)
     )
 
     // Observe window-resize event and redraw all editors.
     window.addEventListener(
       'resize',
       throttle(() => {
-        this._editorContainer.reLayout()
-        this._editorContainer.drawGridsInSight()
-        this._editorContainer.updateDenotationEntitiesWidth()
+        this.#editorContainer.reLayout()
+        this.#editorContainer.drawGridsInSight()
+        this.#editorContainer.updateDenotationEntitiesWidth()
       }, 500)
     )
   }
 
   get nextID() {
-    return this._editorContainer.nextID
+    return this.#editorContainer.nextID
   }
 
   get mousePoint() {
-    return this._mousePoint
+    return this.#mousePoint
   }
 
   registerEditor(element, editor) {
-    this._veil.setObserver(element)
-    this._editorContainer.set(element, editor)
+    this.#veil.setObserver(element)
+    this.#editorContainer.set(element, editor)
   }
 }
