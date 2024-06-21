71a6414fabd6b56386e211d37c0183ffde907e12
ledsun <shigeru.nakajima@gmail.com>
2024-06-21 14:51:19 +0900

    refactor: Fix comment

--- src/lib/Editor/AnnotationModel/SpanInstanceContainer/createRangeToSpan/getRenderingPosition/index.js
+++ src/lib/Editor/AnnotationModel/SpanInstanceContainer/createRangeToSpan/getRenderingPosition/index.js
@@ -5,12 +5,12 @@ export default function (span) {
   const bigBrotherSpan = span.bigBrother
 
   if (bigBrotherSpan) {
-    // The target text arrounded by span is in a textNode after the bigBrotherSpan
+    // The target text enclosed by span is in a textNode after the bigBrotherSpan
     // if bigBrotherSpan exists.
     return getRenderingPositionFromBigBrother(span, bigBrotherSpan)
   } else {
     // There is no big brother if the span is first in the text.
-    // The target text arrounded by span is the first child of parent
+    // The target text enclosed by span is the first child of parent
     // unless bigBrotherSpan exists.
     return getRenderingPositionFromParent(span)
   }
